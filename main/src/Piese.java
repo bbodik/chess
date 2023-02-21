076afc421414d11e2321bf8a7ba67447a9cebed3
bohdan <bbodikk06@gmail.com>
2023-02-21 20:53:37 +0200

    update clas Pises for future obj.

--- main/src/Piese.java
+++ main/src/Piese.java
@@ -1,3 +1,29 @@
 public class Piese {
-    //It is super class of all pieses, i will do it later cause now i`m going to my English lesson
+    private char cordX;
+    private int cordY;
+    private boolean isAlive = true;
+
+    public Piese(char cordX, int cordY) {
+        this.cordX = cordX;
+        this.cordY = cordY;
+    }
+
+    public char getCordX() {
+        return cordX;
+    }
+
+    public void setCordX(char cordX) {
+        this.cordX = cordX;
+    }
+
+    public int getCordY() {
+        return cordY;
+    }
+
+    public void setCordY(int cordY) {
+        this.cordY = cordY;
+    }
+
+    public void move() {
+    }
 }
