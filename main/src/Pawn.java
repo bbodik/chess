3db4a1e3f5f276c539040ab9cf3c509c618e57ff
bohdan <bbodikk06@gmail.com>
2023-02-22 13:20:41 +0200

    updated class Pawn

--- main/src/Pawn.java
+++ main/src/Pawn.java
@@ -1,8 +1,13 @@
+import javax.swing.*;
+
 public class Pawn extends Piese{
     Pawn(char x,int y){
         super(x,y);
     }
-    public static void createPans(){
-
+    public static void createPawns(Panel frame){
+        ImageIcon icon = new ImageIcon("D:/Chessimages/blackPawn.png");
+        JLabel lbl = new JLabel(icon);
+        lbl.setBounds(5,5,45,45);
+        frame.add(lbl);
     }
 }
